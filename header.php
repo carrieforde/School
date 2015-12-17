566442079006eacca0f887b034d320e2f4471726
carrieforde <carrie@carrieforde.com>
2015-12-17 00:41:20 -0800

    Added new header background, began header styling

---
+++ header.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Template for displaying our header.
+ *
+ * This template displays all of the <head> section and everything up until <div id="content">
+ *
+ * @package alcatraz
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+<?php do_action( 'alcatraz_before' ); ?>
+
+<div id="page" class="site">
+
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'alcatraz' ); ?></a>
+
+	<?php do_action( 'alcatraz_before_header' );
+
+		$background_image_id = get_post_thumbnail_id();
+		$background_image_url = wp_get_attachment_image_src( $background_image_id, 'full', false );
+
+	?>
+
+	<div class="header-background" style="background-image: url('<?php echo $background_image_url[0] ?>');">
+
+		<header id="site-header" class="site-header" role="banner">
+
+			<?php do_action( 'alcatraz_before_header_inside' ); ?>
+
+			<div class="header-inner">
+
+				<?php do_action( 'alcatraz_header' ); ?>
+
+			</div>
+
+			<?php if ( has_nav_menu( 'primary' ) ) : ?>
+
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<div class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+					<span class="menu-text"><?php esc_html_e( 'Menu', 'alcatraz' ); ?></span>
+				</div>
+				<div class="menu-overlay"></div>
+				<?php wp_nav_menu( array(
+					'theme_location' => 'primary',
+					'menu_id'        => 'primary-menu',
+					'container_id'   => 'primary-menu-wrap',
+				) ); ?>
+				<div id="mobile-nav-left-swipe-zone"></div>
+				<div id="mobile-nav-right-swipe-zone"></div>
+			</nav>
+
+			<?php endif; ?>
+
+			<?php do_action( 'alcatraz_after_header_inside' ); ?>
+
+		</header>
+
+	</div>
+	<?php do_action( 'alcatraz_after_header' ); ?>
+
+	<?php do_action( 'alcatraz_before_content' ); ?>
+
+	<div id="content" class="site-content">
+
+		<?php do_action( 'alcatraz_before_content_inside' ); ?>
