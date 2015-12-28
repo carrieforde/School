032ae96bb8a4d974723ba7fee4bb53da642b4a78
Carrie Forde <carrie@carrieforde.com>
2015-12-27 21:37:49 -0800

    Remove dependecy on ACF get_field function; use correct sanitization

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -7,36 +7,38 @@
  */
 function school_menu_output() {
 
-	if ( function_exists( 'get_field' ) ) {
+	$menus = get_post_meta( get_the_id(), 'menus', true );
 
-		$menus = get_field( 'menus' );
+	if ( $menus ) {
 
-		if ( $menus ) {
+		echo '<div class="school-menu-wrapper">';
 
-			echo '<div class="school-menu-wrapper">';
+		for ( $i = 0; $i < $menus; $i++ ) {
+			$course = get_post_meta( get_the_id(), 'menus_' . $i . '_course', true );
+			$menu_item = get_post_meta( get_the_id(), 'menus_' . $i . '_menu_item_name', true );
+			$price     = get_post_meta( get_the_id(), 'menus_' . $i . '_price', true );
+			$source    = get_post_meta( get_the_id(), 'menus_' . $i . '_source', true );
+			$description = get_post_meta( get_the_id(), 'menus_' . $i . '_menu_item_description', true );
 
-			foreach ( $menus as $menu ) {
-
-				if ( ! empty( $menu['course'] ) ) {
-					echo '<h2 class="school-menu-course">' . sanitize_text_field( $menu['course'] ) . '</h2>';
-				}
-
-				echo '<h3 class="school-menu-item-heading">';
-
-					echo '<span class"school-menu-item">' . sanitize_text_field( $menu['menu_item_name'] ) . '</span>';
-
-					echo '<span class="school-menu-price">' . sanitize_text_field( $menu['price'] ) . '</span>';
-
-				echo '</h3>';
-
-				if ( ! empty( $menu['source'] ) ) {
-					echo '<p class="school-menu-source">' . sanitize_text_field( $menu['source'] ) . '</p>';
-				}
-
-				echo '<p class="school-menu-description">' . sanitize_text_field( $menu['menu_item_description'] ) . '</p>';
+			if ( ! empty( $course ) ) {
+				echo '<h2 class="school-menu-course">' . esc_attr( $course ) . '</h2>';
 			}
 
-			echo '</div>';
+			echo '<h3 class="school-menu-item-heading">';
+
+				echo '<span class"school-menu-item">' . esc_attr( $menu_item ) . '</span>';
+
+				echo '<span class="school-menu-price">' . esc_attr( $price ) . '</span>';
+
+			echo '</h3>';
+
+			if ( ! empty( $source ) ) {
+				echo '<p class="school-menu-source">' . esc_attr( $source ) . '</p>';
+			}
+
+			echo '<p class="school-menu-description">' . esc_attr( $description ) . '</p>';
 		}
+
+		echo '</div>';
 	}
 }
